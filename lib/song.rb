5692f1387b22caae07aed4da7d2d7cddb5e37532
Doug Kintop <dkintop@hotmail.com>
2019-09-18 19:56:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -38,6 +39,7 @@ class Song
     self.all.each do |song_instance|
       if song_instance.name == song_name
         song_instance
+        binding.pry
       end 
     end
   
